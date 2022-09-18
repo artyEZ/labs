3a5ca42dafa73fc77954082cae2b93081b79a67b
artyEZ <abitur.arty@gmail.com>
2022-09-18 15:37:28 +0400

    Add the first open to create header in file with "w", then change "w" in loop to "a"

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 import csv
 
 URL = " http://www.cbr-xml-daily.ru/archive/2022/09/17/daily_json.js"
+HEADER = ["Day","Excange rate"]
 # html_text = requests.get(url).text
 # json_pars = json.loads(html_text)
 # print(html_text)
@@ -22,6 +23,10 @@ URL = " http://www.cbr-xml-daily.ru/archive/2022/09/17/daily_json.js"
 #     writer = csv.writer(file, delimiter="\t")
 #     writer.writerow(HEADER)
 
+with open("C:/Users/artyo/Desktop/dataset.csv", "w") as file:
+    writer = csv.writer(file)
+    writer.writerow(HEADER)
+
 for i in range(1, 10):
     html_text = requests.get(URL).text
     json_pars = json.loads(html_text)
@@ -30,11 +35,12 @@ for i in range(1, 10):
     final_current_day = re.findall(r'(\d{4}\-\d{2}\-\d{2})', current_day)
 
     current_course = json_pars["Valute"]["USD"]["Value"]
+
     previous_day_url = json_pars["PreviousURL"]
     final_previous_day_url = previous_day_url.replace("//", "")
     switch_current_day_url = "http://" + final_previous_day_url
     # print([str(*final_current_day), current_course])
-    with open("C:/Users/artyo/Desktop/dataset.csv", "w") as file:
+    with open("C:/Users/artyo/Desktop/dataset.csv", "a") as file:
         writer = csv.writer(file)
         writer.writerow([str(*final_current_day), current_course])
 
