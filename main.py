11c6115f30338723b906fb20894bdeff1ebc1cc5
artyEZ <abitur.arty@gmail.com>
2022-09-18 15:43:38 +0400

    Delete commented code to make finish view of file

--- main.py
+++ main.py
@@ -4,24 +4,7 @@ import requests
 import csv
 
 URL = " http://www.cbr-xml-daily.ru/archive/2022/09/17/daily_json.js"
-HEADER = ["Day","Excange rate"]
-# html_text = requests.get(url).text
-# json_pars = json.loads(html_text)
-# print(html_text)
-# current_day = json_pars["Date"]
-#
-# previous_day_url = json_pars["PreviousURL"]
-# final_previous_day_url = previous_day_url.replace('//', '') #ссылка на предыдущий день
-# final_current_day = re.findall(r'(\d{4}\-\d{2}\-\d{2})', current_day)
-# current_course = json_pars["Valute"]["USD"]["Value"]
-#
-# print(str(*final_current_day), current_course)
-# switch_current_day_url = "http://" + final_previous_day_url
-# print(switch_current_day_url)
-
-# with open("C:/Users/artyo/Desktop/dataset.csv", "w", newline='') as file:
-#     writer = csv.writer(file, delimiter="\t")
-#     writer.writerow(HEADER)
+HEADER = ["Day", "Excange rate"]
 
 with open("C:/Users/artyo/Desktop/dataset.csv", "w") as file:
     writer = csv.writer(file, delimiter=";")
@@ -39,7 +22,7 @@ for i in range(1, 10):
     previous_day_url = json_pars["PreviousURL"]
     final_previous_day_url = previous_day_url.replace("//", "")
     switch_current_day_url = "http://" + final_previous_day_url
-    # print([str(*final_current_day), current_course])
+
     with open("C:/Users/artyo/Desktop/dataset.csv", "a") as file:
         writer = csv.writer(file, delimiter=";")
         writer.writerow([str(*final_current_day), current_course])
