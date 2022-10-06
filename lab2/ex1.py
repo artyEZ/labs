20f22eea373a7857a9b82e9b923e3f11aa868351
artyEZ <abitur.arty@gmail.com>
2022-10-06 22:39:54 +0400

    Write splited info into files

--- lab2/ex1.py
+++ lab2/ex1.py
@@ -4,23 +4,25 @@ import mypy
 import re
 
 my_file_for_dates = open("X.csv", "w+")
-my_file_for_data = open("Y.csv", "w+")
-file = "C:/Users/artyo/Desktop/dataset.csv"
+my_file_for_dates.close()
 
-# f = open(file)
-# for line in f:
-#     splited_line = re.split(";", line)
-#     dates_from_splited_file = re.search(r'(\d{4}\-\d{2}\-\d{2})', splited_line[0])
-#     print(dates_from_splited_file, end="")
+my_file_for_data = open("Y.csv", "w+")
+my_file_for_data.close()
+
+file = "C:/Users/artyo/Desktop/dataset.csv"
 
 f = open(file)
 for line in f:
-    f_all_splited = line.split(";")
+    f_all_splited: list = line.split(";")
     for i in range(len(f_all_splited)):
-        dates_from_splited_file = re.findall(r'(\d{4}\-\d{2}\-\d{2})', f_all_splited[i])
-        data_from_splited_file = re.findall(r'(^\d+(?:[\.]\d+)?$)', f_all_splited[i])
-        print(*data_from_splited_file)
-
-
-
-
+        dates_from_splited_file: list = re.findall(r'(\d{4}\-\d{2}\-\d{2})', f_all_splited[i])
+        data_from_splited_file: list = re.findall(r'(^\d+(?:[\.]\d+)?$)', f_all_splited[i])
+        with open("X.csv", "a", newline="") as file:
+            writer = csv.writer(file, delimiter="\n")
+            if len(dates_from_splited_file) != 0:
+                writer.writerow(*[dates_from_splited_file])
+        with open("Y.csv", "a", newline="") as file:
+            writer = csv.writer(file, delimiter="\n")
+            if len(data_from_splited_file) != 0:
+                writer.writerow(*[data_from_splited_file])
+f.close()
