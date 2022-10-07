f07f3a06ed15710bc42568e2b8aee413bd18e9b7
artyEZ <abitur.arty@gmail.com>
2022-10-07 17:46:26 +0400

    Change first write to file one more time to make splited header for using panda in next lab

--- lab1/main.py
+++ lab1/main.py
@@ -5,28 +5,29 @@ import csv
 import pandas as pd
 
 URL = " http://www.cbr-xml-daily.ru/archive/2022/09/17/daily_json.js"
-HEADER = ["Day", "Excange rate"]
+HEADER = ["Day", "Exchange rate"]
 write_to_file = "C:/Users/artyo/Desktop/dataset.csv"
 
 with open(write_to_file, 'w') as file:
-    dw = csv.DictWriter(file, delimiter=';', fieldnames=HEADER)
+    dw = csv.DictWriter(file, delimiter=',',
+                        fieldnames=HEADER)
     dw.writeheader()
 
-while True:
-    html_text = requests.get(URL).text
-    json_pars = json.loads(html_text)
-
-    current_day = json_pars["Date"]
-    final_current_day = re.findall(r'(\d{4}\-\d{2}\-\d{2})', current_day)
-
-    current_course = json_pars["Valute"]["USD"]["Value"]
-
-    previous_day_url = json_pars["PreviousURL"]
-    final_previous_day_url = previous_day_url.replace("//", "")
-    switch_current_day_url = "http://" + final_previous_day_url
-
-    with open(write_to_file, "a", newline="") as file:
-        writer = csv.writer(file,  delimiter=";")
-        writer.writerow([str(*final_current_day), current_course])
-
-    URL = switch_current_day_url
+# while True:
+#     html_text = requests.get(URL).text
+#     json_pars = json.loads(html_text)
+#
+#     current_day = json_pars["Date"]
+#     final_current_day = re.findall(r'(\d{4}\-\d{2}\-\d{2})', current_day)
+#
+#     current_course = json_pars["Valute"]["USD"]["Value"]
+#
+#     previous_day_url = json_pars["PreviousURL"]
+#     final_previous_day_url = previous_day_url.replace("//", "")
+#     switch_current_day_url = "http://" + final_previous_day_url
+#
+#     with open(write_to_file, "a", newline="") as file:
+#         writer = csv.writer(file,  delimiter=";")
+#         writer.writerow([str(*final_current_day), current_course])
+#
+#     URL = switch_current_day_url
