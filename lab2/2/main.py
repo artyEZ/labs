f4d9fe176f59440843bc1ad08a986779a6daf512
artyEZ <abitur.arty@gmail.com>
2022-10-09 01:28:28 +0400

    Take all dates from data column

--- lab2/2/main.py
+++ lab2/2/main.py
@@ -2,13 +2,21 @@ import csv
 from typing import List, Set, Dict
 import mypy
 import re
-import pandas
+import pandas as pd
 
 file = "C:/Users/artyo/Desktop/dataset.csv"
 
-df = pandas.read_csv(file)
-df = df.rename(columns={"Day": "ID"})
-print(df)
+df = pd.read_csv(file)
+columns: list = df["Day"].tolist()
 
+rows = df.index
+start_date = columns[0]
+end_date = columns[-1]
+daterange = pd.date_range(start_date, end_date)
+for i in rows:
+    # if (df["Day"][i] = "2021" ):
+    print((df["Day"][i]))
 
-
+# daterange = pd.date_range(start_date, end_date)
+# len_of_columns = len(columns)
+# for i in len(columns):
