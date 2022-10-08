9b12bc21df215f754e5694f679be3c6dd5627151
artyEZ <abitur.arty@gmail.com>
2022-10-08 23:00:11 +0400

    Clear file

--- lab2/1/main.py
+++ lab2/1/main.py
@@ -1,7 +1,4 @@
 import csv
-from typing import List, Set, Dict
-import mypy
-import re
 import pandas as pd
 
 my_file_for_dates = open("X.csv", "w+")
@@ -12,24 +9,19 @@ my_file_for_data.close()
 
 file = "C:/Users/artyo/Desktop/dataset.csv"
 df = pd.read_csv(file)
-# df_day_coloumn = df["Day"]
-# print(df_day_coloumn)
+
 row_number = len(df.index)
 print(row_number)
 column_day_data = df["Day"].tolist()
 column_data_data = df["Exchange rate"].tolist()
-#
+
 f = open(file)
 for i in range(row_number):
-    # dates_from_splited_file: list = re.findall(r'(\d{4}\-\d{2}\-\d{2})', df_day_coloumn[i])
-    # print(coloumn_day_data[i])
-    # data_from_splited_file: list = re.findall(r'(^\d+(?:[\.]\d+)?$)', df_day_coloumn)
     with open("X.csv", "a", newline="") as file:
         writer = csv.writer(file, delimiter="\n")
-    #     # if len(dates_from_splited_file) != 0:
         writer.writerow([column_day_data[i]])
+
     with open("Y.csv", "a", newline="") as file:
         writer = csv.writer(file, delimiter="\n")
-    #     if len(data_from_splited_file) != 0:
         writer.writerow([column_data_data[i]])
 f.close()
