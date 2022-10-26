8fa537a2a7763687afb834f62366335d6c627deb
artyEZ <abitur.arty@gmail.com>
2022-10-27 00:24:47 +0400

    finish version of data per year sort

--- lab2/1/main.py
+++ lab2/1/main.py
@@ -1,26 +1,46 @@
 import csv
 import pandas as pd
+from typing import NoReturn
 
-my_file_for_dates = open("X.csv", "w+")
-my_file_for_dates.close()
 
-my_file_for_data = open("Y.csv", "w+")
-my_file_for_data.close()
+def formatted_file(input_file: str) -> NoReturn:
+    df = pd.read_csv(input_file)
+    df["Day"] = pd.to_datetime(df.Day, format="%Y-%m-%d")  # превращаю Str to datatime64[ns]
+    df["Date"] = df["Day"].dt.date
+    del df["Day"]
+    print(df)
 
-file = "C:/Users/artyo/Desktop/dataset.csv"
-df = pd.read_csv(file)
 
-row_number = len(df.index)
-column_day_data = df["Day"].tolist()
-column_data_data = df["Exchange rate"].tolist()
+# def write_to_file(input_file: str, output_file: str) -> NoReturn:
+#     df = pd.read_csv(input_file)
+#     row_number = len(df.index)
+#     column_day_data = df["Day"].tolist()
+#     column_data_data = df["Exchange rate"].tolist()
+#
+#     f = open(input_file)
+#     for i in range(row_number):
+#         with open(output_file, "a", newline="") as input_file:
+#             writer = csv.writer(input_file, delimiter="\n")
+#             writer.writerow([column_day_data[i]])
+#     f.close()
 
-f = open(file)
-for i in range(row_number):
-    with open("X.csv", "a", newline="") as file:
-        writer = csv.writer(file, delimiter="\n")
-        writer.writerow([column_day_data[i]])
 
-    with open("Y.csv", "a", newline="") as file:
-        writer = csv.writer(file, delimiter="\n")
-        writer.writerow([column_data_data[i]])
-f.close()
+# РЕАЛИЗАЦИЯ ФУНКЦИИ#
+if __name__ == "__main__":
+    try:
+        file_from = "C:/Users/artyo/Desktop/dataset.csv"
+        file_to = "X.csv"
+        formatted_file(file_from)
+        # write_to_file(file_from, file_to)
+
+    except FileNotFoundError:
+        print("No such file exists!")
+
+    try:
+        file_from = "C:/Users/artyo/Desktop/dataset.csv"
+        formatted_file(file_from)
+        file_to = "Y.csv"
+        # write_to_file(file_from, file_to)
+
+    except FileNotFoundError:
+        print("No such file exists!")
--- lab2/2/main.py
+++ lab2/2/main.py
@@ -1,61 +1,39 @@
-import csv
-from typing import List, Set, Dict
-import mypy
-import re
 import pandas as pd
 from typing import NoReturn
-import calendar
-
-# Могу сравнивать даты
-
-file = "C:/Users/artyo/Desktop/dataset.csv"
-
-df = pd.read_csv(file)
-df["Day"] = pd.to_datetime(df["Day"], format="%Y-%m-%d")  # превращаю Str to datatime64[ns]
-# df["Day"] = df["Day"].dt.date
-
-df["YearsFromDay"] = df["Day"].dt.year
-print(df["YearsFromDay"])
 
 
-# columns: list = df["Day"].tolist()
-# print(columns)
-# rows = df.index
-# start_date = columns[0]
-# end_date = columns[-1]
+def formatted_file(input_file: str) -> pd.DataFrame:
+    df = pd.read_csv(file)
+
+    df["Day"] = pd.to_datetime(df.Day, format="%Y-%m-%d")
+    df["Day1"] = df["Day"].dt.date  # превращаю Str to datatime64[ns]
+    df["Date"] = df["Day"].dt.year
+    return df
 
 
-# for i in len(a):
-#     print(a[i])
+def write_to_file(input_file: str, year: int) -> NoReturn:
+    df = formatted_file(input_file)
 
-# if start_date > end_date:
-
-# a = pd.bdate_range(start=start_date, end=end_date)
-# print(a)
-# print(type(start_date), end_date)
-# daterange = pd.date_range(start_date, end_date, freq="D")
-# print(daterange)
-
-# for i in rows:
-#     # if (df["Day"][i] = "2021" ):
-#     row_info_day = (df.iloc[i]["Day"])
-#     print(type(row_info_day))
-
-# daterange = pd.date_range(start_date, end_date)
-# len_of_columns = len(columns)
-# for i in len(columns):
+    df = df[df["Date"] == year]
+    data = str(df["Day1"].iloc[0]).replace('-', '') + "_" + str(df["Day1"].iloc[df.shape[0] - 1]).replace('-', '')
+    del df["Date"]
+    del df["Day1"]
+    df.to_csv(data + ".csv", index=False)
 
 
-# start_date = datetime.date(2020, 1, 1)
-# end_date = datetime.date(2020, 1, 4)
-# delta = datetime.timedelta(days=1)
-#
-# while start_date <= end_date:
-#     print(start_date)
-#     start_date += delta
-def year_from_date(input_file: str) -> NoReturn:
-    data = pd.read_csv(input_file)
-    data["Day"] = pd.to_datetime(df["Day"], format="%Y-%m-%d")  # превращаю Str to datatime64[ns]
-    data["YearsFromDay"] = df["Day"].dt.year
-#
-# def name_for_file() -> str:
+def range_of_date(input_file: str) -> list :
+    df = formatted_file(input_file)
+
+    start_range = df["Date"].iat[0]
+    end_range = df["Date"].iat[-1]
+    return [start_range, end_range]
+
+
+if __name__ == "__main__":
+
+    file = "C:/Users/artyo/Desktop/dataset.csv"
+
+    range_of_years = range_of_date(file)
+    for years in range(range_of_years[0], range_of_years[1] - 1, -1):
+        write_to_file(file, years)
+
