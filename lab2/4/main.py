50ffa4e9b2b3beed0681d4ab8c9a26d185b3e38a
artyEZ <abitur.arty@gmail.com>
2022-11-04 17:59:59 +0400

    Add function for divided by years

--- lab2/4/main.py
+++ lab2/4/main.py
@@ -1,31 +1,39 @@
+import numpy
 import pandas as pd
 import datetime
 import os
+from typing import Union
+import autopep8
 
 
-def get_data_xy(input_file_x: str, input_file_y: str, date: datetime.date):
+def formatted_file(input_file: str) -> pd.DataFrame:
+    df = pd.read_csv(input_file)
+    df["Day"] = pd.to_datetime(df.Day, format="%Y-%m-%d")
+    return df
+
+
+def get_data_xy(input_file: str, input_file_x: str, input_file_y: str,
+                date: datetime.date) -> Union[numpy.float64, None]:
     if os.path.exists(input_file_x) and os.path.exists(input_file_y):
+
+        formatted_file(input_file)
         df_x = pd.read_csv(input_file_x)
         df_y = pd.read_csv(input_file_y)
-        print(df_x)
-
-        for i in range(df_x.shape[0]):
-            if df_x["Data"].iloc[i].replace("-", "") == str(date).replace("-", ""):
+        index = -1
+        for i in range(0, df_x.shape[0], 1):
+            if df_x["Day"].iloc[i].replace("-", "") == str(date).replace("-", ""):
                 index = i
                 break
         if index >= 0:
-            return df_y.iloc[index]
+            return df_y.iloc[index]["Exchange rate"]
         return None
-
-        # if ()
-
     raise FileNotFoundError
 
 
 if __name__ == "__main__":
+    file = "C:/Users/artyo/Desktop/dataset.csv"
     file_x = "C:/Users/artyo/PycharmProjects/labs/lab2/1/X.csv"
     file_y = "C:/Users/artyo/PycharmProjects/labs/lab2/1/Y.csv"
 
-    exist_date = datetime.date(2020, 2, 3)
-    # print(exist_date)
-    get_data_xy(file_x, file_y, exist_date)
+    exist_date = datetime.date(2022, 9, 15)
+    print(get_data_xy(file, file_x, file_y, exist_date))
