4b0f5b112c696ba97f35ea29f6c809a7e95323de
artyEZ <abitur.arty@gmail.com>
2022-11-06 18:23:11 +0400

    Make iterator class for the third and fourth type of data

--- lab2/5/main.py
+++ lab2/5/main.py
@@ -36,11 +36,34 @@ class DateIteratorXY:
             return self.xf.loc[self.counter - 1]["Day"], self.yf.loc[self.counter - 1]["Exchange rate"]
 
 
+class DateIteratorYearOrWeek:
+
+    def __init__(self, name):
+
+        self.counter = 0
+        self.df = pd.DataFrame()
+        for root, dirs, files in os.walk(name):
+            for filename in files[-2: 0 : -1]:
+                data = os.path.join(root, filename)
+                yf = pd.read_csv(data)
+                self.df = pd.concat([self.df, yf], ignore_index=True)
+
+    def __next__(self) -> tuple:
+        if self.counter == self.df.shape[0]:
+            raise StopIteration
+        elif self.counter < self.df.shape[0]:
+            self.counter += 1
+            return self.df.loc[self.counter - 1]["Day"], self.df.loc[self.counter - 1]["Exchange rate"]
+
+
+
 if __name__ == "__main__":
     # obj = DateIterator()
     # while True:
     #     print(next(obj))
-    obj = DateIteratorXY()
+    # obj = DateIteratorXY()
+    # while True:
+    #     print(next(obj))
+    obj = DateIteratorYearOrWeek("C:/Users/artyo/PycharmProjects/labs/lab2/2/")
     while True:
         print(next(obj))
-
