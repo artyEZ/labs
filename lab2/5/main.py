997acbbd6273ec1666a6e5dffeb0ee215a9cf3b5
artyEZ <abitur.arty@gmail.com>
2022-11-06 18:21:40 +0400

    Make iterator class for second type of data

--- lab2/5/main.py
+++ lab2/5/main.py
@@ -19,11 +19,28 @@ class DateIterator:
         raise FileNotFoundError
 
 
+class DateIteratorXY:
 
+    def __init__(self):
+
+        self.counter = 0
+        self.xf = pd.read_csv("C:/Users/artyo/PycharmProjects/labs/lab2/1/X.csv")
+        self.yf = pd.read_csv("C:/Users/artyo/PycharmProjects/labs/lab2/1/Y.csv")
+
+    def __next__(self) -> tuple:
+        if self.counter == self.xf.shape[0]:
+            raise StopIteration
+
+        elif self.counter < self.xf.shape[0]:
+            self.counter += 1
+            return self.xf.loc[self.counter - 1]["Day"], self.yf.loc[self.counter - 1]["Exchange rate"]
 
 
 if __name__ == "__main__":
-    obj = DateIterator()
+    # obj = DateIterator()
+    # while True:
+    #     print(next(obj))
+    obj = DateIteratorXY()
     while True:
         print(next(obj))
 
