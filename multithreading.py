cb069d488706dcaf3fc4f2a7ce79127144a6c8ae
artyEZ <abitur.arty@gmail.com>
2022-11-07 17:41:10 +0400

    try to compare time of function with multithreading and without

--- multithreading.py
+++ multithreading.py
@@ -3,25 +3,41 @@ import time
 import numpy as np
 
 
-def find_hypotenuse() -> tuple:
+def find_hypotenuse_with_theading() -> tuple:
 
     cathetus_1 = float(input("Input the first cathetus: "))
     cathetus_2 = float(input("Input the second cathetus: "))
 
     start_time = time.time()
-    with concurrent.futures.ProcessPoolExecutor() as executor:
+    with concurrent.futures.ProcessPoolExecutor(max_workers=6) as executor:
 
         cathetus_square_1 = executor.submit(np.power, cathetus_1, 2)
         cathetus_square_2 = executor.submit(np.power, cathetus_2, 2)
 
         hypotenuse = executor.submit(np.power, cathetus_square_1.result() + cathetus_square_2.result(), 0.5)
 
-        print("--- %s seconds ---" % (time.time() - start_time))
+        print("--- %.5f seconds ---" % (time.time() - start_time))
 
-    return '%.3f' % hypotenuse, np.power(cathetus_1, 2), np.power(cathetus_2, 2)
-
-
-
-    if __name__ == "__main__":
+    return "%.3f" % hypotenuse.result(), "%.3f" % np.power(cathetus_1, 2), "%.3f" % np.power(cathetus_2, 2)
 
 
+def find_hypotenuse() -> tuple:
+    cathetus_1 = float(input("Input the first cathetus: "))
+    cathetus_2 = float(input("Input the second cathetus: "))
+
+    start_time = time.time()
+
+    cathetus_square_1 = np.power(cathetus_1, 2)
+    cathetus_square_2 = np.power(cathetus_2, 2)
+
+    hypotenuse = np.power(cathetus_square_1 + cathetus_square_2, 0.5)
+
+    print("--- %.5f seconds ---" % (time.time() - start_time))
+
+    return "%.3f" % hypotenuse, "%.3f" % np.power(cathetus_1, 2), "%.3f" % np.power(cathetus_2, 2)
+
+
+if __name__ == "__main__":
+    print(find_hypotenuse_with_theading())
+    print()
+    print(find_hypotenuse())
