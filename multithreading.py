b6b7842d3aea68c2f82c0686098d639c261f9300
artyEZ <abitur.arty@gmail.com>
2022-11-08 13:13:14 +0400

    Change input method using terminal

--- multithreading.py
+++ multithreading.py
@@ -1,24 +1,28 @@
 import concurrent.futures
 import time
 import numpy as np
+import argparse
 
 
 def find_hypotenuse_with_theading() -> tuple:
 
-    cathetus_1 = float(input("Input the first cathetus: "))
-    cathetus_2 = float(input("Input the second cathetus: "))
+    parser = argparse.ArgumentParser(description="Find hypotenuse")
+    parser.add_argument("cathetus_square_1", type=float, help="Input first cathetus: ")
+    parser.add_argument("cathetus_square_2", type=float, help="Input second cathetus: ")
+    args = parser.parse_args()
 
     start_time = time.time()
     with concurrent.futures.ProcessPoolExecutor(max_workers=12) as executor:
 
-        cathetus_square_1 = executor.submit(np.power, cathetus_1, 2)
-        cathetus_square_2 = executor.submit(np.power, cathetus_2, 2)
+        cathetus_square_1 = executor.submit(np.power, args.cathetus_square_1, 2)
+        cathetus_square_2 = executor.submit(np.power, args.cathetus_square_2, 2)
 
         hypotenuse = executor.submit(np.power, cathetus_square_1.result() + cathetus_square_2.result(), 0.5)
 
         print("--- %.5f seconds ---" % (time.time() - start_time))
 
-    return "%.3f" % hypotenuse.result(), "%.3f" % np.power(cathetus_1, 2), "%.3f" % np.power(cathetus_2, 2)
+    return "%.3f" % hypotenuse.result(), "%.3f" % np.power(args.cathetus_square_1, 2), \
+           "%.3f" % np.power(args.cathetus_square_2, 2)
 
 
 def find_hypotenuse_with_sleep() -> tuple:
@@ -56,7 +60,3 @@ def find_hypotenuse() -> tuple:
 
 if __name__ == "__main__":
     print(find_hypotenuse_with_theading())
-    print()
-    print(find_hypotenuse_with_sleep())
-    print()
-    print(find_hypotenuse())
