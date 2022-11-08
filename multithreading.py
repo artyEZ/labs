2273d6f9a5715d5a2f6b780c8a97a7c74ce443f1
artyEZ <abitur.arty@gmail.com>
2022-11-08 13:15:26 +0400

    Change all method using input from terminal

--- multithreading.py
+++ multithreading.py
@@ -26,36 +26,40 @@ def find_hypotenuse_with_theading() -> tuple:
 
 
 def find_hypotenuse_with_sleep() -> tuple:
-    cathetus_1 = float(input("Input the first cathetus: "))
-    cathetus_2 = float(input("Input the second cathetus: "))
+    parser = argparse.ArgumentParser(description="Find hypotenuse")
+    parser.add_argument("cathetus_square_1", type=float, help="Input first cathetus: ")
+    parser.add_argument("cathetus_square_2", type=float, help="Input second cathetus: ")
+    args = parser.parse_args()
 
     start_time = time.time()
     time.sleep(2)
 
-    cathetus_square_1 = np.power(cathetus_1, 2)
-    cathetus_square_2 = np.power(cathetus_2, 2)
+    cathetus_square_1 = np.power(args.cathetus_1, 2)
+    cathetus_square_2 = np.power(args.cathetus_2, 2)
 
     hypotenuse = np.power(cathetus_square_1 + cathetus_square_2, 0.5)
 
     print("--- %.5f seconds ---" % (time.time() - start_time))
 
-    return "%.3f" % hypotenuse, "%.3f" % np.power(cathetus_1, 2), "%.3f" % np.power(cathetus_2, 2)
+    return "%.3f" % hypotenuse, "%.3f" % np.power(args.cathetus_1, 2), "%.3f" % np.power(args.cathetus_2, 2)
 
 
 def find_hypotenuse() -> tuple:
-    cathetus_1 = float(input("Input the first cathetus: "))
-    cathetus_2 = float(input("Input the second cathetus: "))
+    parser = argparse.ArgumentParser(description="Find hypotenuse")
+    parser.add_argument("cathetus_square_1", type=float, help="Input first cathetus: ")
+    parser.add_argument("cathetus_square_2", type=float, help="Input second cathetus: ")
+    args = parser.parse_args()
 
     start_time = time.time()
 
-    cathetus_square_1 = np.power(cathetus_1, 2)
-    cathetus_square_2 = np.power(cathetus_2, 2)
+    cathetus_square_1 = np.power(args.cathetus_1, 2)
+    cathetus_square_2 = np.power(args.cathetus_2, 2)
 
     hypotenuse = np.power(cathetus_square_1 + cathetus_square_2, 0.5)
 
     print("--- %.5f seconds ---" % (time.time() - start_time))
 
-    return "%.3f" % hypotenuse, "%.3f" % np.power(cathetus_1, 2), "%.3f" % np.power(cathetus_2, 2)
+    return "%.3f" % hypotenuse, "%.3f" % np.power(args.cathetus_1, 2), "%.3f" % np.power(args.cathetus_2, 2)
 
 
 if __name__ == "__main__":
