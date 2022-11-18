0f68a6b2eddc72ba973e6e73e23a3fa5b1c77437
artyEZ <abitur.arty@gmail.com>
2022-11-18 12:28:57 +0400

    Add checking exit from the application and centering the main window

--- lab3/main_window.py
+++ lab3/main_window.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QMessageBox, QDesktopWidget
 from PyQt5.QtGui import QIcon
 
 import sys
@@ -12,11 +12,30 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(750, 400, 300, 220)
+        self.resize(250, 150)
+        self.center()
+
         self.setWindowTitle("Icon")
         self.setWindowIcon(QIcon("dollar.jpg"))
 
         self.show()
+
+    def closeEvent(self, event):
+
+        reply = QMessageBox.question(self, "Message",
+                                     "Are you sure to quit?", QMessageBox.Yes |
+                                     QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 
 if __name__ == '__main__':
