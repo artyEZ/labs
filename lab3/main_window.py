2b53ea2e56234600f9242177a030de6f7c4661c6
artyEZ <abitur.arty@gmail.com>
2022-11-18 12:24:17 +0400

    make class and add icon

--- lab3/main_window.py
+++ lab3/main_window.py
@@ -1,16 +1,25 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from PyQt5.QtGui import QIcon
 
-
 import sys
 
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(750, 400, 300, 220)
+        self.setWindowTitle("Icon")
+        self.setWindowIcon(QIcon("dollar.jpg"))
+
+        self.show()
+
+
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.setWindowTitle('Simple')
-    w.show()
-
+    ex = Example()
     sys.exit(app.exec_())
-
